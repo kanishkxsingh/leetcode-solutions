01b7f5ec52b649b4f9939515b430f8d80c287452
Kanishk Singh <singhkanishk2005@gmail.com>
2026-08-02 10:20:17 +0530

    Time: 1 ms (100%), Space: 47.3 MB (65.63%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,78 +1,26 @@
 class Solution {
-
-    void merge(int a[], int l, int m, int r)
-    {
-
-      	int n1 = m - l + 1;
-        int n2 = r - m;
-
-        int L[] = new int[n1];
-        int R[] = new int[n2];
-
-        for (int i = 0; i < n1; ++i)
-            L[i] = a[l + i];
-
-      	for (int j = 0; j < n2; ++j)
-            R[j] = a[m + 1 + j];
-
-        // Merge the temp arrays
-        // Initial indexes of first and second subarrays
-        int i = 0, j = 0;
-
-        int k = l;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                a[k] = L[i];
-                i++;
-            }
-            else {
-                a[k] = R[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < n1) {
-            a[k] = L[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            a[k] = R[j];
-            j++;
-            k++;
-        }
-    }
-
-        void Mergesort(int a[], int l, int r)
-    {
-        if (l < r) {
-          
-            int m = (l + r) / 2;
-
-            // Sort first and second halves
-            Mergesort(a, l, m);
-            Mergesort(a, m + 1, r);
-
-            // Merge the sorted halves
-            merge(a, l, m, r);
-        }
-    }
-
         public int[] sortedSquares(int[] nums) {
-       //int answer []=new int[nums.length]; //new array created to store the answers
+            int answer[]=new int[nums.length];  // new array created for answer
+            int i=0;
+            int j=nums.length-1;  //dont use for loop bcoz we want conditional increement of the pointers(negative numbers can be there)
+                                  // however not guaranteed that every array would have them...so input array can be positive too
+            int k=answer.length-1; //pointer for answer array storing
+            while(i<=j)
+            {
+                if(nums[i]*nums[i]>nums[j]*nums[j])
+                {
+                    answer[k--]=nums[i]*nums[i];
+                    i++;  //i increases only if that elements value stored in answer
+                }
+                else
+                {
+                    answer[k--]=nums[j]*nums[j];
+                    j--;  //j decreases only if that elements value is stored in answer
+                }
 
-       for(int i=0;i<nums.length;i++) //squaring the elements of the array
-       {
-        nums[i]=nums[i]*nums[i];
-       } 
+                // above also handles equal values on both sides since pointer moves conditionally and not simultaneously
+            } 
 
-       Mergesort(nums,0,nums.length-1);  //sorting the squared array
-
-       return nums;
-
-       // TC = O(NLOGN) + O(N)....BASICALLY OF SORTING AND SQUARING
-       // SC = O(1).....NO EXTRA SPACE USED APART FROM QUESTION ARRAY
-    }
+            return answer;  //return the final answer array                     
+    }                             
 }
