09134044eda1b2119a23fe9f622f36a5f684bc94
Kanishk Singh <singhkanishk2005@gmail.com>
2026-08-13 18:45:44 +0530

    Time: 3 ms (18.15%), Space: 47.6 MB (76.64%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,18 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j=0;
-        for(int i=0;i<nums.length;i++) //j tracks and stops at 0 to be replaced and i checks every element to replace j 0 with i non 0
+        int counter=0;
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]!=0) //j moves only when the element is non zero...i moves every step
+            if(nums[i]==0)
             {
-               int temp=nums[i];
-               nums[i]=0;
-               nums[j]=temp;
-               j++;
+                counter++;
             }
-            //even when if condition not true, no loss i increases and j remains at 0 waiting to be replaced and increemented
+            else if(nums[i]!=0 && counter>0)
+            {
+                int temp=nums[i];
+                nums[i]=0;
+                nums[i-counter]=temp;
+            }
         }
     }
 }
-
-//classic 2 pointer approach jaha har ek 0 ko uske agle non 0 element se swap krna hai aur isme j ko 0 par rukake ensure krte hai
-//consecutive 0 sequence mein 1st 0 of sequence hi replace ho...
-//tc is o(n) and space is o(1)
