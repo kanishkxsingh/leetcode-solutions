23c02c5cff822ca1e168627fc6f9ec751b50192e
Kanishk Singh <singhkanishk2005@gmail.com>
2026-08-13 18:48:24 +0530

    Time: 2 ms (92.03%), Space: 47.6 MB (88.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -7,7 +7,7 @@ class Solution {
             {
                 counter++;
             }
-            else if(counter>0)
+            else if(counter>0)  //jus adding nums[i]!=0 in this check condition runtime went from 2 to 3 ms 
             {
                 int temp=nums[i];
                 nums[i]=0;
